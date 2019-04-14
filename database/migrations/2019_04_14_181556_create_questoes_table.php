747e12a9d292eee5a201dbcdda64d8a6cc3b3c62
Wallison Ferreira <contato.sanfer@gmail.com>
2019-04-14 13:39:09 -0500

    Tabelas de questões adicionadas

---
+++ database/migrations/2019_04_14_181556_create_questoes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestoesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questoes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('descricao_problema');
+            $table->integer('competencia_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questoes');
+    }
+}
---
+++ database/migrations/2019_04_14_181724_create_alternativas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAlternativasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('alternativas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('descricao_alternativa')->nullable();
+            $table->text('path_alternativa')->nullable();
+            $table->boolean('imagem')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('alternativas');
+    }
+}
